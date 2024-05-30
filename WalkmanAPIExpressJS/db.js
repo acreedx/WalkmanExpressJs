60fa149ede46b5bef39247f72c021eba0a9c665f
Adrian Herrera <adrianhlinares@gmail.com>
2024-05-30 04:42:38 -0400

    api arreglada

--- WalkmanAPIExpressJS/db.js
+++ WalkmanAPIExpressJS/db.js
@@ -1,17 +1,16 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const key = process.env.MONGO_URI.toString();
-const connectDB = async () => {
+const uri = process.env.MONGO_URI.toString();
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
+async function run() {
   try {
-    await mongoose.connect(key, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(uri, clientOptions);
     console.log("Conexión exitosa a la base de datos");
   } catch (error) {
     console.error("Error al conectar a la base de datos:", error);
     process.exit(1); // Salir de la aplicación si la conexión falla
   }
-};
-
-module.exports = connectDB;
+}
+run().catch(console.dir);
